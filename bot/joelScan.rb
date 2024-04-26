4ac97b4e1d5f87e9d9d30b40a08093bcdc5c1cef
venorrak <jaudet199@gmail.com>
2024-04-26 19:27:35 -0400

    fixed bugs creating users in DB

--- bot/joelScan.rb
+++ bot/joelScan.rb
@@ -278,7 +278,6 @@ def loginIRC(oauthToken)
         @socket.puts("JOIN ##{channel}")
         @joinedChannels << channel
     end
-    @socket.puts("JOIN #venorrak")
 end
 
 #---------------------------------------------------------------------------------------------
@@ -372,7 +371,7 @@ while @running do
                             pfp = nil
                             bgp = nil
                             #add the user to the database
-                            response = $APItwitch.get("/helix/users?login=#{name}") do |req|
+                            response = response = $APItwitch.get("/helix/users?login=#{name}") do |req|
                                 req.headers["Authorization"] = "Bearer #{@APItoken}"
                                 req.headers["Client-Id"] = @client_id
                             end
@@ -429,6 +428,7 @@ while @running do
                                 begin
                                     rep = JSON.parse(response.body)
                                     rep["data"].each do |user|
+					p user
                                         twitch_id = user["id"]
                                         pfp = user["profile_image_url"]
                                         bgp = user["offline_image_url"]
@@ -442,7 +442,9 @@ while @running do
                                     pfp_id = @client.query("SELECT id FROM pictures WHERE url = '#{pfp}';").first["id"]
                                     bgp_id = @client.query("SELECT id FROM pictures WHERE url = '#{bgp}';").first["id"]
                                     @client.query("INSERT INTO users VALUES (DEFAULT, '#{twitch_id}', '#{pfp_id}', '#{bgp_id}', '#{message[:command][:channel].delete_prefix("#")}', '#{DateTime.now.strftime("%Y-%m-%d")}');")
+                                    @client.query("INSERT INTO users VALUES (DEFAULT, '#{twitch_id}', '#{pfp_id}', '#{bgp_id}', '#{message[:command][:channel].delete_prefix("#")}', '#{DateTime.now.strftime("%Y-%m-%d")}');")
                                     #get the id of the new user
+                                    @client.query("SELECT id FROM users WHERE name = '#{message[:command][:channel].delete_prefix("#")}';").each do |row|
                                     @client.query("SELECT id FROM users WHERE name = '#{message[:command][:channel].delete_prefix("#")}';").each do |row|
                                         user_id = row["id"]
                                     end
